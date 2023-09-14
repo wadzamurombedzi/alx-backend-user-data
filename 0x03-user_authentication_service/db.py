19939efa3566c0acd63980af6c9aadc2a4598526
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2023-09-14 18:29:37 +0000

    User authentication service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -11,65 +11,65 @@ from user import Base, User
 from typing import TypeVar
 
 VALID_FIELDS = ['id', 'email', 'hashed_password', 'session_id',
-                        'reset_token']
+                'reset_token']
 
 
 class DB:
+    """
+    DB class.
+    """
+
+    def __init__(self):
         """
-            DB class.
-                """
+        Constructor.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
 
-                    def __init__(self):
-                                """
-                                        Constructor.
-                                                """
-                                                        self._engine = create_engine("sqlite:///a.db", echo=False)
-                                                                Base.metadata.drop_all(self._engine)
-                                                                        Base.metadata.create_all(self._engine)
-                                                                                self.__session = None
+    @property
+    def _session(self):
+        """
+        _session.
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
 
-                                                                                    @property
-                                                                                        def _session(self):
-                                                                                                    """
-                                                                                                            _session.
-                                                                                                                    """
-                                                                                                                            if self.__session is None:
-                                                                                                                                            DBSession = sessionmaker(bind=self._engine)
-                                                                                                                                                        self.__session = DBSession()
-                                                                                                                                                                return self.__session
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        add_user.
+        """
+        if not email or not hashed_password:
+            return
+        user = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(user)
+        session.commit()
+        return user
 
-                                                                                                                                                                def add_user(self, email: str, hashed_password: str) -> User:
-                                                                                                                                                                            """
-                                                                                                                                                                                    add_user.
-                                                                                                                                                                                            """
-                                                                                                                                                                                                    if not email or not hashed_password:
-                                                                                                                                                                                                                    return
-                                                                                                                                                                                                                        user = User(email=email, hashed_password=hashed_password)
-                                                                                                                                                                                                                                session = self._session
-                                                                                                                                                                                                                                        session.add(user)
-                                                                                                                                                                                                                                                session.commit()
-                                                                                                                                                                                                                                                        return user
+    def find_user_by(self, **kwargs) -> User:
+        """
+        find_user_by.
+        """
+        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
+            raise InvalidRequestError
+        session = self._session
+        try:
+            return session.query(User).filter_by(**kwargs).one()
+        except Exception:
+            raise NoResultFound
 
-                                                                                                                                                                                                                                                        def find_user_by(self, **kwargs) -> User:
-                                                                                                                                                                                                                                                                    """
-                                                                                                                                                                                                                                                                            find_user_by.
-                                                                                                                                                                                                                                                                                    """
-                                                                                                                                                                                                                                                                                            if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
-                                                                                                                                                                                                                                                                                                            raise InvalidRequestError
-                                                                                                                                                                                                                                                                                                                session = self._session
-                                                                                                                                                                                                                                                                                                                        try:
-                                                                                                                                                                                                                                                                                                                                        return session.query(User).filter_by(**kwargs).one()
-                                                                                                                                                                                                                                                                                                                                            except Exception:
-                                                                                                                                                                                                                                                                                                                                                            raise NoResultFound
-
-                                                                                                                                                                                                                                                                                                                                                            def update_user(self, user_id: int, **kwargs) -> None:
-                                                                                                                                                                                                                                                                                                                                                                        """
-                                                                                                                                                                                                                                                                                                                                                                                update_user.
-                                                                                                                                                                                                                                                                                                                                                                                        """
-                                                                                                                                                                                                                                                                                                                                                                                                session = self._session
-                                                                                                                                                                                                                                                                                                                                                                                                        user = self.find_user_by(id=user_id)
-                                                                                                                                                                                                                                                                                                                                                                                                                for k, v in kwargs.items():
-                                                                                                                                                                                                                                                                                                                                                                                                                                if k not in VALID_FIELDS:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                    raise ValueError
-                                                                                                                                                                                                                                                                                                                                                                                                                                                            setattr(user, k, v)
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                    session.commit()
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        update_user.
+        """
+        session = self._session
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if k not in VALID_FIELDS:
+                raise ValueError
+            setattr(user, k, v)
+        session.commit()
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,20 +9,20 @@ Base = declarative_base()
 
 
 class User(Base):
+    """
+    User class.
+    """
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
         """
-            User class.
-                """
-
-                    __tablename__ = 'users'
-
-                        id = Column(Integer, primary_key=True)
-                            email = Column(String(250), nullable=False)
-                                hashed_password = Column(String(250), nullable=False)
-                                    session_id = Column(String(250), nullable=True)
-                                        reset_token = Column(String(250), nullable=True)
-
-                                            def __repr__(self):
-                                                        """
-                                                                String rep.
-                                                                        """
-                                                                                return f"User: id={self.id}"
+        String rep.
+        """
+        return f"User: id={self.id}"
